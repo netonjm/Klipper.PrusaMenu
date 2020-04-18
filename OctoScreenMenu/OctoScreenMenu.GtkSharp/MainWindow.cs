f264d0f4de33c4962671c5d031f54eb5bda54b32
Jose Medrano <josmed@microsoft.com>
2020-04-18 21:35:37 +0200

    test

--- OctoScreenMenu/OctoScreenMenu.GtkSharp/MainWindow.cs
+++ OctoScreenMenu/OctoScreenMenu.GtkSharp/MainWindow.cs
@@ -1,31 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using Gdk;
 using Gtk;
 using OctoScreenMenu;
 
 public partial class MainWindow : Gtk.Window
 {
+    Gtk.Image backgroundImageView;
+    readonly string imagePath;
+    readonly DrawingArea darea;
+
+    readonly Fixed absoluteContainer;
 
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
 
-        var path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-        var imagePath = System.IO.Path.Combine(path, "CentreLIT.png");
+        absoluteContainer = new Fixed();
+        Add(absoluteContainer);
 
+        var directoryImagePath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        imagePath = System.IO.Path.Combine(directoryImagePath, "CentreLIT.png");
 
-        var h = new Fixed();
+        var pixBuf = new Pixbuf(imagePath, 500, 500, false);
+        backgroundImageView = new Gtk.Image(pixBuf);
 
-        var btn = new Button();
-        btn.Relief = ReliefStyle.Half;
-        btn.BorderWidth = 0;
-        Gtk.Image dd = new Image(imagePath);
-        btn.Image = dd;
-        btn.ModifyBg(StateType.Selected, new Gdk.Color (0,0,0));
-        h.Put(btn, 20, 20);
-        this.Add(h);
+        absoluteContainer.Put(backgroundImageView, 0, 0);
+
+        var redArrowLeft = System.IO.Path.Combine(directoryImagePath, "red-arrow-left-icon-225849.png");
+        var redArrowLeftImage = new Gtk.Image(redArrowLeft);
+
+        var currentButton = new Button();
+        currentButton.Label = "dsadsdsasdsda";
+        currentButton.WidthRequest = 100;
+        currentButton.HeightRequest = 100;
+        currentButton.Relief = ReliefStyle.Half;
+        absoluteContainer.Put(currentButton, 30, 30);
+
+        var button2 = new Button();
+        button2.WidthRequest = 100;
+        button2.HeightRequest = 100;
+        button2.Relief = ReliefStyle.Half;
+        absoluteContainer.Put(button2, 60, 60);
+
         ShowAll();
+    }
+
+    //private void BackgraundDrawingAreaExposeEvent(object o, ExposeEventArgs args)
+    //{
+    //    var area = (DrawingArea)o;
+    //    Cairo.Context cr = Gdk.CairoHelper.Create(area.GdkWindow);
+    //    var imageSurface = new Cairo.ImageSurface(imagePath);
+    //    float w = imageSurface.Width;
+    //    float h = imageSurface.Height;
+    //    cr.Scale(Allocation.Width / w, Allocation.Height / h);
+    //    CairoHelper.SetSourcePixbuf(cr, pi, 0, 0);
+    //    cr.Paint();
+    //    cr.Fill();
+    //    ((IDisposable)cr.Target).Dispose();
+    //    ((IDisposable)cr).Dispose();
+    //}
+
+    void RefreshImage ()
+    {
+        //darea.WidthRequest = Allocation.Width;
+        //darea.HeightRequest = Allocation.Height;
+    }
+
+    protected override bool OnConfigureEvent(EventConfigure evnt)
+    {
+        var configure = base.OnConfigureEvent(evnt);
+
+        RefreshImage ();
+
+        return configure;
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
---
+++ OctoScreenMenu/OctoScreenMenu.IoT/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Gtk;
+
+namespace OctoScreenMenu.IoT
+{
+    public class MainWindow : Window
+    {
+        int count = 0;
+        readonly Button button;
+
+        public MainWindow() : base(WindowType.Toplevel)
+        {
+            button = new Button("Click Me");
+            button.Clicked += OnBtnActionClicked;
+            Add(button);
+            ShowAll();
+        }
+
+        protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+            a.RetVal = true;
+        }
+
+        protected void OnBtnActionClicked(object sender, EventArgs e)
+        {
+            button.Label = string.Format("You pressed {0} times.", ++count);
+        }
+    }
+}
